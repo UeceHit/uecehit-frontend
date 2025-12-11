0ea2f95448455f26b9ff637231ea88ba202d3225
Pedro Ulisses <pedro.ulisses@aluno.uece.br>
2025-12-11 16:04:00 -0300

    fix: requests

--- src/app/login-everyone/page.jsx
+++ src/app/login-everyone/page.jsx
@@ -25,7 +25,7 @@ export default function LoginPage() {
     setLoading(true);
 
     try {
-      const response = await fetch("/api/auth/login", {
+      const response = await fetch("https://api.uecehit.com.br/api/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, password }),
